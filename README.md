75af7f78aeac09487358b15efbb5c3a2dc51049d
zomg830 <matt.faubel@gmail.com>
2018-12-18 17:19:03 -0600

    Added several more assets, a difficulty setting, and tweak the bootstrap formatting

--- README.md
+++ README.md
@@ -12,12 +12,16 @@ In this assignment, I created a "Word Guess" game similar to the classic game "H
 
 ### How to Play
 
-* To play the game, simply load the web page and begin typing to guess the word. Use the clue as a guideline and if you have no background in The Expanse lore, feel free to check out the [wiki page](http://expanse.wikia.com/wiki/The_Expanse_Wiki).
+* To play the game, simply load the web page and begin typing to guess the word. Use the hint as a guideline and if you have no background in The Expanse lore, feel free to check out the [wiki page](http://expanse.wikia.com/wiki/The_Expanse_Wiki).
 
-* If you don't guess the correct answer, the web page will reset and another random word and clue will be generated. If you successfully guess the clue, an image and link to the wiki will be generated, click the reset button to generate another clue.
+* If you don't guess the correct answer by exhausting your remaining guesses, the web page will reset and another random word and hint will be generated. If you successfully guess the answer, an image and link to the wiki will be generated, click the reset button to clear the character image + wiki link and generate another answer.
+
+* After a answer has been used once it will not appear again in the same game session. Refreshing the page will refresh the answer bank but also reset your wins. Try to get as high of a score as possible in the same game session!
+
+* There is no need to include spaces in your guesses, and all guesses are strictly alphabet characters. Attempting to use punctuation or letters will lead to some... interesting results being displayed in the "Incorrect Guesses" field.
 
 ### Credits and Acknowledgements
 
-* I would like to thank the instructors and TAs at The Coding Bootcamp at UT Austin for introducing me to the concepts and knowledge needed to create this game. 
+* I would like to thank the instructors and TAs at The Coding Bootcamp at UT Austin for introducing me to the concepts and knowledge needed to create this game. Google, w3schools.com, and Mozilla MDN also deserve honorable mentions.
 
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,28 @@
 //Initialize answers array with corresponding hints array. Important to note that the answer should be in all caps and that any spaces should be represented with a hyphen.
-var answerArray = ["JAMES-HOLDEN", "NAOMI-NAGATA", "AMOS-BURTON", "ALEX-KAMAL", "ROCINANTE", "CHRISJEN-AVASARALA", "SADAVIR-ERRINWRIGHT", "OUTER-PLANETS-ALLIANCE", "STAR-HELIX-SECURITY", "JOSEPHUS-MILLER", "PROTOMOLECULE","CANTERBURY", "MARS-CONGRESSIONAL-REPUBLIC","ANDERSON-DAWES","TYCHO-STATION"];
-var hintArray = ["Former XO of the ice hauler Canterbury.", "Former chief engineer of the ice hauler Canterbury.", "Raised as a street urchin in Baltimore.", "Former pilot with the MCRN, honorably discharged after 20 years.", "Corvette-class light frigate seized as legitimate salvage from the MCRN.", "Served as the United Nations Deputy Undersecretary of Executive Administration.", "Earth-born co-conspirator of Jules-Pierre Mao during the Eros incident.", "Separatist organization representing the various peoples of the asteroid belt and gas-giant moons.", "Private security force contracted to Ceres Station.", "Ceres-born detective rarely seen without his porkpie hat.", "Infectious agent of extra-terrestrial origin first discovered on Phoebe.","Retooled water hauler, destroyed while on transit back to Ceres Station.", "Governing body of 'The Red Planet'.", "Worked as the OPA liaison on Ceres during the Eros Incident.","Largest mobile construction platform in the Sol system."];
+// var answerArray = ["JAMES-HOLDEN"];
+var answerArray = ["JAMES-HOLDEN", "NAOMI-NAGATA", "AMOS-BURTON", "ALEX-KAMAL", "ROCINANTE", "CHRISJEN-AVASARALA", "SADAVIR-ERRINWRIGHT", "OUTER-PLANETS-ALLIANCE", "STAR-HELIX-SECURITY", "JOSEPHUS-MILLER", "PROTOMOLECULE","CANTERBURY", "MARS-CONGRESSIONAL-REPUBLIC","ANDERSON-DAWES","TYCHO-STATION", "JULIETTE-ANDROMEDA-MAO","DONNAGER","ROBERTA-DRAPER","NAUVOO","SOLOMON-EPSTEIN","PRAXIDIKE-MENG"];
+var hintArray = ["Former XO of the ice hauler Canterbury.", "Former chief engineer of the ice hauler Canterbury.", "Raised as a street urchin in Baltimore.", "Former pilot with the MCRN, honorably discharged after 20 years.", "Corvette-class light frigate seized as legitimate salvage from the MCRN.", "Served as the United Nations Deputy Undersecretary of Executive Administration.", "Earth-born co-conspirator of Jules-Pierre Mao during the Eros incident.", "Separatist organization representing the various peoples of the asteroid belt and gas-giant moons.", "Private security force contracted to Ceres Station.", "Ceres-born detective rarely seen without his porkpie hat.", "Infectious agent of extra-terrestrial origin first discovered on Phoebe.","Retooled water hauler, destroyed while on transit back to Ceres Station.", "Governing body of 'The Red Planet'.", "Worked as the OPA liaison on Ceres during the Eros Incident.","Largest mobile construction platform in the Sol system.", "Oldest child of business magnate Jules-Pierre and pilot of the racing pinnace Razorback.","Flagship of the MCRN's Jupiter fleet.","Martian marine and Gunnery Sergeant of the 2nd Marine Expeditionary Force.","Generation ship constructed at Tycho Station for the Church of Latter Day Saints.","Inventor whose namesake drive allowed humanity to colonize the Asteroid Belt and outer planets.","Chief Botanist of the RMD-Southern soy farm project on Ganymede."];
 var incorrect = "";
 
 //Array store to display the URLs and pictures upon completing a specified clue
-var urlArray = ["http://expanse.wikia.com/wiki/Jim_Holden_(Books)","http://expanse.wikia.com/wiki/Naomi_Nagata_(Books)", "http://expanse.wikia.com/wiki/Amos_Burton_(Books)", "http://expanse.wikia.com/wiki/Alex_Kamal_(Books)", "http://expanse.wikia.com/wiki/Rocinante_(Books)", "http://expanse.wikia.com/wiki/Chrisjen_Avasarala_(Books)", "http://expanse.wikia.com/wiki/Sadavir_Errinwright_(Books)", "http://expanse.wikia.com/wiki/Outer_Planets_Alliance", "http://expanse.wikia.com/wiki/Star_Helix_Security", "http://expanse.wikia.com/wiki/Josephus_Miller_(Books)", "http://expanse.wikia.com/wiki/Protomolecule","http://expanse.wikia.com/wiki/Canterbury_(Books)", "http://expanse.wikia.com/wiki/Martian_Congressional_Republic","http://expanse.wikia.com/wiki/Anderson_Dawes_(Books)","http://expanse.wikia.com/wiki/Tycho_Station"];
-var picArray = ["assets/images/Holden.jpg", "assets/images/Nagata.jpg", "assets/images/Burton.jpg", "assets/images/Kamal.jpg", "assets/images/Rocinante.jpg", "assets/images/Avasarala.jpg", "assets/images/Errinwright.jpg", "assets/images/OPA.png", "assets/images/Star-Helix.png", "assets/images/Miller.jpg", "assets/images/Protomolecule.jpg","assets/images/Cant.jpg", "assets/images/MCR.png","assets/images/Dawes.jpg","assets/images/Tycho.png"];
+var urlArray = ["http://expanse.wikia.com/wiki/Jim_Holden_(Books)","http://expanse.wikia.com/wiki/Naomi_Nagata_(Books)", "http://expanse.wikia.com/wiki/Amos_Burton_(Books)", "http://expanse.wikia.com/wiki/Alex_Kamal_(Books)", "http://expanse.wikia.com/wiki/Rocinante_(Books)", "http://expanse.wikia.com/wiki/Chrisjen_Avasarala_(Books)", "http://expanse.wikia.com/wiki/Sadavir_Errinwright_(Books)", "http://expanse.wikia.com/wiki/Outer_Planets_Alliance", "http://expanse.wikia.com/wiki/Star_Helix_Security", "http://expanse.wikia.com/wiki/Josephus_Miller_(Books)", "http://expanse.wikia.com/wiki/Protomolecule","http://expanse.wikia.com/wiki/Canterbury_(Books)", "http://expanse.wikia.com/wiki/Martian_Congressional_Republic","http://expanse.wikia.com/wiki/Anderson_Dawes_(Books)","http://expanse.wikia.com/wiki/Tycho_Station","http://expanse.wikia.com/wiki/Julie_Mao_(Books)","http://expanse.wikia.com/wiki/Donnager","http://expanse.wikia.com/wiki/Bobbie_Draper_(Books)","http://expanse.wikia.com/wiki/Nauvoo","http://expanse.wikia.com/wiki/Solomon_Epstein_(Books)","http://expanse.wikia.com/wiki/Praxidike_Meng_(Books)"];
+var picArray = ["assets/images/Holden.jpg", "assets/images/Nagata.jpg", "assets/images/Burton.jpg", "assets/images/Kamal.jpg", "assets/images/Rocinante.jpg", "assets/images/Avasarala.jpg", "assets/images/Errinwright.jpg", "assets/images/OPA.png", "assets/images/Star-Helix.png", "assets/images/Miller.jpg", "assets/images/Protomolecule.jpg","assets/images/Cant.jpg", "assets/images/MCR.png","assets/images/Dawes.jpg","assets/images/Tycho.png","assets/images/Julie_Mao.jpg","assets/images/Donnager.jpg","assets/images/Bobbie.jpg","assets/images/Nauvoo.jpg","assets/images/Epstein.jpg","assets/images/Prax.jpg"];
 
 //Attempting to create game as object, as specified in readme
 var guessGame = {
     hint: "",
     currentAnswer: "",
     wins: 0,
-    guessesLeft: 10,
     lettersUsed: "",
     answerDiv: "",
+    guessesLeft: 0,
     victoryCounter: 0,
     splitAnswer: "",
     hiddenAnswer: "",
     spaces: 0,
     winPic: "",
     winUrl: "",
+    difficulty: "",
     initialize: function() {
         //Generates a random number to pull out an answer and hint
         var r = Math.floor(Math.random() * answerArray.length);
@@ -29,6 +31,18 @@ var guessGame = {
         guessGame.currentAnswer = this.currentAnswer;
         guessGame.winPic = picArray[r];
         guessGame.winUrl = urlArray[r];
+
+        //Ends game if no clues remaining
+        if(answerArray.length === 0){
+            alert('You have completed the game! The page will refresh and allow you to play again! Can you beat your previous high score?')
+            guessGame.reload();
+        }
+
+        //Remove answer and hint once pulled
+        answerArray.splice(r,1);
+        hintArray.splice(r,1);
+        urlArray.splice(r,1);
+        picArray.splice(r,1);
 
         //Using an onload event handler to initialize some HTML content
         window.onload = function(){
@@ -42,11 +56,11 @@ var guessGame = {
         }
 
         //Calculates the number of spaces for the win condition logic
-            for (var i = 0; i < guessGame.currentAnswer.length; i++){
-                var character = guessGame.currentAnswer[i];
-                if(character === "-"){
-                    guessGame.spaces++;
-                }
+        for (var i = 0; i < guessGame.currentAnswer.length; i++){
+            var character = guessGame.currentAnswer[i];
+            if(character === "-"){
+                guessGame.spaces++;
+            }
         }
 
 
@@ -61,9 +75,9 @@ var guessGame = {
             }
         }
 
-        console.log("Length of 'hiddenAnswer' array: " + guessGame.hiddenAnswer.length);
+        //console.log("Length of 'hiddenAnswer' array: " + guessGame.hiddenAnswer.length);
         
-        // Splits the answer array?
+        // Splits the answer array
         guessGame.splitAnswer = guessGame.currentAnswer.split("");
         document.getElementById("answer").innerHTML = (guessGame.hiddenAnswer);
         for (var i = 0; i < guessGame.splitAnswer.length; i++){
@@ -80,17 +94,17 @@ var guessGame = {
         guessGame.spaces = 0;
         guessGame.lettersUsed = "";
         guessGame.splitAnswer = "";
-        guessGame.guessesLeft = 10;
+        guessGame.guessesLeft = guessGame.setDifficulty();
         incorrect = "";
         userGuess="";
         guessGame.initialize();
         guessGame.update();
         document.getElementById("characterPic").innerHTML = "";
         document.getElementById("characterUrl").innerHTML = "";
-        document.getElementById("resetBtn").innerHTML = "";
-    
-    //Updates the HTML window 
+        document.getElementById("resetBtn").innerHTML = ""; 
     },
+
+    //Updates the HTML window
     update: function (){
         var disp = 
         "<p>Hint: " + guessGame.hint + "</p>" + 
@@ -99,25 +113,62 @@ var guessGame = {
         "<p>Incorrect Guesses: " + incorrect + "</p>"
         document.getElementById("disp").innerHTML = disp;
         document.getElementById("answer").innerHTML = "<h3>" + (guessGame.hiddenAnswer.join(" ")) + "</h3>";
-        console.log(guessGame.hiddenAnswer);
+        //console.log(guessGame.hiddenAnswer);
     },
+
+    //Displays the pictures from picArray and creates a button that links to the wiki page in urlArray. Also creates reset button.
     winDisplay: function(){
         var winDispPic = 
         "<img src=" + guessGame.winPic + ">";
         document.getElementById("characterPic").innerHTML = winDispPic;
         var winDispUrl = 
-        '<a href=' + guessGame.winUrl + ' target="_blank" class="btn-sm btn-primary">Click to visit the wiki!</a>';
+        '<a href=' + guessGame.winUrl + ' target="_blank" class="btn-sm btn-secondary">Click to visit the wiki!</a>';
         document.getElementById("characterUrl").innerHTML = winDispUrl;
         var resetBtn = 
-        '<a href="#" class="btn-sm btn-primary" onclick="guessGame.reset()">Click to reset!</a>';
+        '<a href="#" class="btn-sm btn-secondary" onclick="guessGame.reset()">Click for the next word!</a>';
         document.getElementById("resetBtn").innerHTML = resetBtn;
-    }  
+    },
+
+    //Reloads the game after exhausting all the answers
+    reload: function(){
+        window.location.reload();
+    },
+
+    //Prompts the user to input the difficulty at the initialization of the game session
+    initDifficulty: function(){
+        var difficulty = prompt('Select your difficulty by typing "Ensign" (beginner - 10 guesses), "Captain" (medium - 8 guesses), or "Admiral" (hard - 6 guesses)');
+        if (difficulty==="Ensign" || difficulty==="ensign"){
+            guessGame.difficulty = "1";
+        }
+        if (difficulty==="Captain" || difficulty==="captain"){
+            guessGame.difficulty = "2";
+        }
+        if (difficulty==="Admiral" || difficulty==="admiral"){
+            guessGame.difficulty = "3";
+        }
+    },
+
+    //Function is called by the guessesLeft property to set the number of incorrect guesses allowed based on difficulty
+    setDifficulty: function(){
+        if (guessGame.difficulty==="1"){
+            return 10;
+        }
+        if (guessGame.difficulty==="2"){
+            return 8;
+        }
+        if (guessGame.difficulty==="3"){
+            return 6;
+        }
+    },
 }
 //End of guessGame object
 
 
 //Initialize the game with some console logs for debugging
 guessGame.initialize();
+guessGame.initDifficulty();
+guessGame.setDifficulty();
+guessGame.guessesLeft = guessGame.setDifficulty(),
 console.log("Current Answer: " + guessGame.currentAnswer);
 console.log("Current Hint: " + guessGame.hint);
 console.log("Split Answer: " + guessGame.splitAnswer);
@@ -125,6 +176,7 @@ console.log("Split Answer Length: " + guessGame.splitAnswer.length);
 console.log("Number of Spaces: " + guessGame.spaces);
 console.log(guessGame.winPic);
 console.log(guessGame.winUrl);
+console.log("Guesses based on difficulty: " + guessGame.setDifficulty())
 
 //Primary logic for game
 document.onkeyup = function(event) {
